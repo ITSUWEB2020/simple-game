224b81035e5e40aa427d0bcdf7dbb51c4d5fc28d
Roman Rak <itsuweb2020@gmail.com>
2021-06-15 17:02:27 +0300

    com
    com

---
+++ src/components/Results.vue
@@ -0,0 +1,29 @@
+<template>
+  <p>Reaction time: {{score}} ms</p>
+  <p class="rank"> {{ rank }}</p>
+</template>
+
+<script>
+export default {
+     props: ['score'],
+     date(){
+         return{
+             rank: null
+         }
+     },
+     mounted(){
+         if (this.score < 250) {
+             this.rank = "Ninja Fingers"
+         } else if (this.score < 400){
+             this.rank = "Rapid Reflexes"
+         } else {
+             this.rank = "snail pace..."
+         }
+         console.log(this.rank)
+     }
+}
+</script>
+
+<style>
+
+</style>
